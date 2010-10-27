9cf4db8d4b3f5886ad5d076c4cb411fe322fa1bd
Ben Mabey <ben@benmabey.com>
2010-10-27 16:39:41 -0600

    updates deps and README.  Details below.
      * Updated the deps to current repos and bumped the javac plugin.
      * Added LeadTune's maven repo so weka can be pulled down with `lein deps`.
      * Removed incanter temporarliy. The most recent clojars version wasn't working for me.
      * Updated README with new lein instructions.
      * Made note in README where things didn't work as advertised.
      * Verified that `lein test` passes with these deps.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject clj-ml "0.0.3-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
+  :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
   :java-source-path "src/java"
   :javac-fork "true"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [lein-javac "0.0.2-SNAPSHOT"]
-                 [incanter/incanter-full "1.0-master-SNAPSHOT"]
-                 [com.mongodb/mongo "1.0"]
-                 [weka/weka "3.6.2"]]
-  :dev-dependencies [[autodoc "0.7.0"]]
+                 [org.mongodb/mongo-java-driver "1.0"]
+                 [lt/weka "3.6.3"]]
+  :dev-dependencies [[autodoc "0.7.0"]
+                     [lein-javac "1.2.1-SNAPSHOT"]]
   :autodoc { :name "clj-ml", :page-title "clj-ml machine learning Clojure's style"
              :author "Antonio Garrote <antoniogarrote@gmail.com>"
              :copyright "2010 (c) Antonio Garrote, under the MIT license"})
