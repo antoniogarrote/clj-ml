b1130cf80da5491e4bcbf859d7e96730f525c1a2
Ben Mabey <ben@benmabey.com>
2011-10-07 13:47:51 -0600

    releases v0.1.3 with attribute selection wrappers

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leadtune/clj-ml "0.1.3-SNAPSHOT"
+(defproject com.leadtune/clj-ml "0.1.3"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
   :java-source-path "src/java"
