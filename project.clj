8c533ebfd9b4d8627d5ab559ca9fdc4de6fcae90
Ben Mabey <ben@benmabey.com>
2012-01-23 16:47:04 -0700

    bumps back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leadtune/clj-ml "0.1.5"
+(defproject com.leadtune/clj-ml "0.1.6-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
   :aoc "src/java"
